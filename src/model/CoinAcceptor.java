604b3b87a9a2489df5b8b951b9ed6fbd4a50e72a
Eldarfox <eldarkemelov200@gmail.com>
2025-12-11 17:43:02 +0600

    add universal interface and remake CoinAcceptor

--- src/model/CoinAcceptor.java
+++ src/model/CoinAcceptor.java
@@ -1,17 +1,34 @@
 package model;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements Payment {
+
     private int amount;
 
-    public CoinAcceptor(int amount) {
-        this.amount = amount;
+    public CoinAcceptor(int initialAmount) {
+        this.amount = initialAmount;
     }
 
-    public int getAmount() {
+    @Override
+    public int getBalance() {
         return amount;
     }
 
-    public void setAmount(int amount) {
-        this.amount = amount;
+    @Override
+    public void addPayment(int amount) {
+        this.amount += amount;
+    }
+
+    @Override
+    public boolean deduct(int amount) {
+        if (this.amount >= amount) {
+            this.amount -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return "Монетоприемник";
     }
 }
